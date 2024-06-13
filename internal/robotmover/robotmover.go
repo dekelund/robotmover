c6254a44257ab206f78037b16788a0e2f2c07e4f
Daniel Ekelund <contact@danielekelund.info>
2024-06-13 21:23:02 +0200

    feat: add roomlimits, and robotmover

--- internal/robotmover/robotmover.go
+++ internal/robotmover/robotmover.go
@@ -1,1 +1,31 @@
 package robotmover
+
+type direction string
+
+const (
+	North direction = "n"
+	East  direction = "e"
+	South direction = "s"
+	West  direction = "w"
+)
+
+type Position struct {
+	X, Y      uint
+	Direction direction
+}
+
+type RoomLimits struct {
+	X, Y uint
+}
+
+type RobotMover struct {
+	limits          RoomLimits
+	currentPosition Position
+}
+
+func New(l RoomLimits, p Position) *RobotMover {
+	return &RobotMover{
+		limits:          l,
+		currentPosition: p,
+	}
+}
--- internal/robotmover/robotmover_test.go
+++ internal/robotmover/robotmover_test.go
@@ -1,1 +1,26 @@
 package robotmover_test
+
+import (
+	"testing"
+
+	"github.com/dekelund/robotmover/internal/robotmover"
+)
+
+func TestNew(t *testing.T) {
+	limits := robotmover.RoomLimits{
+		X: 10,
+		Y: 10,
+	}
+
+	position := robotmover.Position{
+		X: 0,
+		Y: 0,
+		Direction: robotmover.South,
+	}
+
+	mover := robotmover.New(limits, position)
+
+	if mover == nil {
+		t.Fatal("unexpected nil")
+	}
+}
